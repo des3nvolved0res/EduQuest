fa1d8c7458c7b7b2b7ede6be5f4e4e874c0d5823
Alderan SOARES DA SILVA <projetoeduquest@gmail.com>
2026-05-25 18:38:49 -0300

    remove .env do repositório

---
+++ app.config.js
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+
+export default {
+  expo: {
+    name: "EduQuest",
+    slug: "eduquest",
+    version: "1.0.0",
+    orientation: "portrait",
+    scheme: "eduquest",
+    platforms: ["ios", "android"],
+    extra: {
+      firebaseApiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    }
+  }
+}
